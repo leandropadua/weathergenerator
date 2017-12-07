47017b02fafdd9c9a6e8aa509dfe8f185a41b353
Leandro Padua <leandro.padua@servian.com>
2017-12-07 22:50:07 +1100

    Initial Commit

---
+++ src/main/java/au/com/commbank/weathergenerator/App.java
@@ -0,0 +1,13 @@
+package au.com.commbank.weathergenerator;
+
+/**
+ * Weather Generator Main Class
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Weather version 0.0.1" );
+    }
+}
